4bdf235634f52999085be825a0deb7ca74cae04a
Cristiano <cristianovelkan@gmail.com>
2021-01-19 23:33:51 -0300

    feat: created user email confirmation flow

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -109,7 +109,7 @@ $app->routeMiddleware([
 
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
 $app->register(Laravel\Passport\PassportServiceProvider::class);
 $app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 $app->register(\Thedevsaddam\LumenRouteList\LumenRouteListServiceProvider::class);
